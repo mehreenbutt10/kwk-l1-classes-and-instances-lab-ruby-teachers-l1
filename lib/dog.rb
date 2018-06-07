995ea19017ea0f13f4684aab6209cc57479370ed
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 00:50:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog
+  def initalize
+  end
+end
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,7 @@
 # person.rb
+class Person
+  def initalize (adele_goldberg, alan_kay)
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end
+end
