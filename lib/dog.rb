b9c9875034a7d65d0409fe1c6211b10a6adafbc9
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 00:56:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # dog.rb
+class Dog
+  def initialize(dog)
+    @dog = dog
+  end
+  
+  def dog
+    @dog
+  end
+end
+
+dog = []
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
+
+dog.push(fido, snoopy, lassie)
+puts dog
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person
+  def initialize(person)
+    @person = person
+  end
+  
+  def person
+    @person
+  end
+  
+end
+
+person = []
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Person.new("alan_kay")
+
+person.push(adele_goldberg, alan_kay)
+puts person
