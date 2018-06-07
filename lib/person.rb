6e950cf695b03293f834ee2b64a1037b90111280
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 00:53:31 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 # person.rb
 class Person
-  def initalize (adele_goldberg, alan_kay)
-    @adele_goldberg = adele_goldberg
-    @alan_kay = alan_kay
+  def initialize(singer)
+    @singer = singer
   end
 end
+
